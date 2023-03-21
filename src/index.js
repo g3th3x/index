9ad5029b6d38870f58dd1c23ada0de1e424698af
g3th3x <g3th3x@gmail.com>
2023-03-22 02:49:29 +0300

    Small changes in Feedback section, added button up and e.t.c.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,56 @@
-const submitBtn = document.querySelector("#submit-button");
+"use strict";
+
+const submitBtn = document.querySelector("#submitButton");
 submitBtn.addEventListener("click", () => {
   console.log("submit");
 });
+
+const checkbox = document.querySelector("#check");
+checkbox.addEventListener("change", () => {
+  checkbox.checked == true
+    ? submitBtn.classList.remove("disabled")
+    : submitBtn.classList.add("disabled");
+});
+
+(function () {
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.prototype.slice.call(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (e) => {
+        if (!form.checkValidity()) {
+          e.preventDefault();
+          e.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+const btnUp = {
+  el: document.querySelector(".gh-btn-up"),
+  show() {
+    this.el.classList.remove("gh-btn-up-hide");
+  },
+  hide() {
+    this.el.classList.add("gh-btn-up-hide");
+  },
+  addEventListener() {
+    window.addEventListener("scroll", () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      scrollY > 300 ? this.show() : this.hide();
+    });
+    document.querySelector(".gh-btn-up").onclick = () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    };
+  },
+};
+
+btnUp.addEventListener();
